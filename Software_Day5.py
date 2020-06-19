cb3eaa73441ed0b63fda4aca212b389f7de31b0d
Swastik Samal <51965359+Swastik-tsk@users.noreply.github.com>
2020-06-20 01:43:38 +0530

    Create Software_Day5.py
    Easter Egg was EUREKA.
    i too have an egg for you.

---
+++ Software_Day5.py
@@ -0,0 +1,17 @@
+a_binary_string = "01000110 01010101 01000011 01001011 01011001 01001111 01010101 00100001"
+
+binary_values = a_binary_string.split()
+
+ascii_string = ""
+for binary_value in binary_values:
+    an_integer = int(binary_value, 2)
+
+
+
+    ascii_character = chr(an_integer)
+
+
+    ascii_string += ascii_character
+
+print(ascii_string)
+
